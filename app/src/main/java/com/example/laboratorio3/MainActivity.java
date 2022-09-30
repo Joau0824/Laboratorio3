2e13b73d68cf492097118e22ad3a3f08ecff5a2d
Joau0824 <a20182162@pucp.edu.pe>
2022-09-30 08:35:05 -0500

    commit 1

--- app/src/main/java/com/example/laboratorio3/MainActivity.java
+++ app/src/main/java/com/example/laboratorio3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.laboratorio3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Ambulancias Mascotín");
+    }
+
+    public void registro (View view){
+        Intent intent = new Intent (this, MainActivityRegistro.class);
+        startActivity(intent);
+
+    }
+
+    public void emergencia (View view){
+        Intent intent = new Intent (this, MainActivityEmergencia.class);
+        startActivity(intent);
+
+    }
+
+    public void historial (View view){
+        Intent intent = new Intent (this, MainActivityHistorial.class);
+        startActivity(intent);
+
     }
 }
