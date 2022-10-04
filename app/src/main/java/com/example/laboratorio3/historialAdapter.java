e4738c8ec8de6c26610457a8039a91327de5e3d2
NicoG <guillen.nicole@pucp.edu.pe>
2022-10-04 00:13:05 -0500

    historial_avance

---
+++ app/src/main/java/com/example/laboratorio3/historialAdapter.java
@@ -0,0 +1,38 @@
+package com.example.laboratorio3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class historialAdapter extends RecyclerView.Adapter<historialAdapter.HistorialViewHolder> {
+    private Context context;
+
+    public class HistorialViewHolder extends RecyclerView.ViewHolder{
+
+        public HistorialViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public HistorialViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.historial_view, parent, false);
+        return new HistorialViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistorialViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
